40989a501380d860950d6561bbfc876ae627a397
Alexandre Plateau <lexplt.dev@gmail.com>
2025-04-26 19:10:18 +0200

    fix(year annotate): adding logs and fixing path to files

--- year-annotate/gnuplot.py
+++ year-annotate/gnuplot.py
@@ -16,6 +16,8 @@ with open("data/codeage.plot") as f:
 current_year = datetime.datetime.now().year
 start_year = 2019
 
+print(f"Current year: ${current_year}")
+
 if f'"≥ {current_year}"' not in content or True:
     start = content[:content.index("# START PLOT") + 13]
     end = content[content.index("# END PLOT"):]
@@ -24,15 +26,17 @@ if f'"≥ {current_year}"' not in content or True:
         f.write(start)
         f.write("plot \\\n")
         for y in range(current_year, start_year - 1, -1):
-            f.write(f" 'codeage.csv' using 1:{y - start_year + 2} axes x1y2 with filledcurves above title")
+            f.write(f" 'data/codeage.csv' using 1:{y - start_year + 2} axes x1y2 with filledcurves above title")
             f.write(f' "≥ {y}"')
             if y != start_year:
                 f.write(", \\\n")
             else:
                 f.write("\n")
         f.write(end)
+    print("Updated codeage.plot")
 
 output = subprocess.run(["gnuplot", "-p", "data/codeage.plot"], capture_output=True)
 with open("data/codeage.svg", "w") as f:
     f.write(output.stdout.decode('utf-8').strip())
+print("Updated codeage.svg")
 
